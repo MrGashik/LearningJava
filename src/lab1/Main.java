1e5779fb687eb3e95e3970560fe747fbe7979ef9
MrGashik <mr.alex.chesh@mail.ru>
2023-02-27 22:50:56 +0300

    Added a stand strategy

--- src/lab1/Main.java
+++ src/lab1/Main.java
@@ -4,7 +4,17 @@ import lab1.strategy.*;
 
 public class Main {
     public static void main(String[] args) {
-        Hero hero = new Hero(new StrategyWalk());
+        System.out.println("Stand - 0 step");
+        System.out.println("Swim - 1 step");
+        System.out.println("Walk - 2 step");
+        System.out.println("Pony - 3 step");
+        System.out.println("Horse - 4 step");
+        System.out.println("Fly - 5 step\n");
+
+        Hero hero = new Hero(new StrategyStand());
+        hero.move();
+
+        hero.changeStrategyMove(new StrategyWalk());
         hero.move();
 
         hero.changeStrategyMove(new StrategyFly());
---
+++ src/lab1/strategy/StrategyStand.java
@@ -0,0 +1,13 @@
+package lab1.strategy;
+
+public class StrategyStand implements MoveStrategy {
+    @Override
+    public int move() {
+        return 0;
+    }
+
+    @Override
+    public String heroStatus() {
+        return "The hero decided to rest.";
+    }
+}
